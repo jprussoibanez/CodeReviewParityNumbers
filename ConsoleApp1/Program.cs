9216758d23dbd85256c0c0b42287d00902472c9d
Juan Pablo Russo <jprussoibanez@gmail.com>
2017-07-01 15:30:41 -0300

    Original console application to code review

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace TESTAPPLICATION
+{
+    public class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            for (count = 0; count < 25; count++)
+            {
+                Console.Write(count);
+                if (_evenIfTake3())
+                    Console.Write(" is even");
+                else
+                    Console.Write(" isn't even");
+                Console.Write(Environment.NewLine);
+            }
+            Console.ReadKey();
+
+
+        }
+
+        public static int count;
+
+        public static bool _evenIfTake3()
+        {
+            var iseven = ((count - 3) % 2) == 0 ? true : false;
+            return iseven;
+        }
+
+
+
+    }
+}
